e5c4b17456b53ce19c6ba5c5685e812cbab18082
JosueFlores777 <aaaaaveroo@gmail.com>
2022-10-30 16:53:09 -0600

    primer avance del portafolio personal Josue

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('.nav'),
+        navList = nav.querySelectorAll('li'),
+        totalnavali = navList.length;
+          for (let i = 0; i < totalnavali; i++) {
+           const a=navList[i].querySelector("a");
+           a.addEventListener("click",function(){
+            for(let j=0; j<totalnavali;j++){
+                navList[j].querySelector("a").classList.remove("active");
+              
+              }
+              this.classList.add("active");
+              showSection(this);
+           })
+          }
+function showSection(element){
+  console.log(element.getAttribute("href").split("#"));
+}
